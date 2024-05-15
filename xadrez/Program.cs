18bf8757c93863eafc8bb652ca8e92dc1720cdfb
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-05-15 15:40:51 -0300

    Criacao classes Tabuleiro/Peca/Cor

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -5,9 +5,7 @@ using xadrez.tabuleiro;
 namespace xadrez {
     class Program {
         static void Main(string[] args) {
-            Posicao P = new Posicao(3, 4);
-
-            Console.WriteLine("Posicao: " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
     }
