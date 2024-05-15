5ad735e82104f99ead5b6e22c8cf1e664b130093
Vinicius Faria <vinicius.f4ria@gmail.com>
2024-05-15 16:28:01 -0300

    implementacao das cores na tela

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Globalization;
-using xadrez.jogoXadrez;
 using xadrez.tabuleiro;
+using xadrez.jogoXadrez;
 
 namespace xadrez {
     class Program {
         static void Main(string[] args) {
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Console.WriteLine(pos.toPosicao());
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 1));
+            tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 2));
+            tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(5, 2));
+
+            Tela.ImprimirTabuleiro(tab);
 
 
             Console.ReadLine();
